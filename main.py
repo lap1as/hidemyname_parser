5d49ef60679d83835da6ba4567c2b076a7a40543
Laplas <46395687+lap1as@users.noreply.github.com>
2022-04-06 22:27:46 +0300

    make it better

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ i = 0
 
 def configure_url():
     """"Proxy configuration"""
-    #main url
+    # main url
     main_url = 'https://hidemy.name/ua/proxy-list/'
-    #proxy speed
+    # proxy speed
     proxy_speed = f'?maxtime={input("Enter proxy speed: ")}'
     if proxy_speed == '0':
         proxy_speed = int('')
     elif proxy_speed == '\n':
         proxy_speed = int('')
-    #proxy anonimity
+    # proxy anonimity
     proxy_anon_input = input('Anonimity lvl (0-3) or (all) :')
     if proxy_anon_input == '0':
         proxy_anon = '&anon=1'
@@ -31,7 +31,7 @@ def configure_url():
         print('Invalid input')
         quit()
         return
-    #proxy type
+    # proxy type
     proxy_type_input = input('Type (h) for http, (hs) for https, (4) for socks4, (5) for socks5 or (all):')
     if proxy_type_input == 'h':
         proxy_type = '&type=h'
@@ -47,16 +47,16 @@ def configure_url():
         print('Invalid input')
         return
     all_url = main_url + proxy_speed + proxy_type + proxy_anon + '#list'
-    print("-"*100)
+    print("-" * 100)
     print(all_url)
     return all_url
 
 
-def next_page(driver,work):
-    ''''Next page'''
+def next_page(driver, work):
+    """Next page"""
     try:
-        driver.find_element(By.CLASS_NAME, value="next_array").click()
-        work = True
+        if driver.find_element(By.CLASS_NAME, value="next_array").click():
+            work = True
     except Exception as ex:
         print(ex)
         print('No more pages')
@@ -71,30 +71,37 @@ def driver():
     driver.maximize_window()
     return driver
 
-def parse_page(driver,work,proxy_num):
+
+def parse_page(driver, work, proxy_num):
     ''''Parse data'''
     driver.get(configure_url())
-    time.sleep(2)
-    #get all proxies
+    time.sleep(1)
+
+    # get all proxies
     while work == True:
-        get_proxy = driver.find_elements(By.TAG_NAME, value="tbody")[0].find_elements(By.TAG_NAME, value="tr")[proxy_num].find_elements(By.TAG_NAME, value="td")[0].text
-        get_port = driver.find_elements(By.TAG_NAME, value="tbody")[0].find_elements(By.TAG_NAME, value="tr")[proxy_num].find_elements(By.TAG_NAME, value="td")[1].text
+        try:
+            get_proxy = driver.find_elements(By.TAG_NAME, value="tbody")[0].find_elements(By.TAG_NAME, value="tr")[
+                proxy_num].find_elements(By.TAG_NAME, value="td")[0].text
+            get_port = driver.find_elements(By.TAG_NAME, value="tbody")[0].find_elements(By.TAG_NAME, value="tr")[
+                proxy_num].find_elements(By.TAG_NAME, value="td")[1].text
 
-        with open('./proxy/proxies.txt', 'a') as f:
-            f.write(get_proxy + ':' + get_port + '\n')
-            f.close()
+            with open('./proxy/proxies.txt', 'a') as f:
+                f.write(get_proxy + ':' + get_port + '\n')
+                f.close()
 
-        proxy_num += 1
+            proxy_num += 1
 
-        if proxy_num == len(driver.find_elements(By.TAG_NAME, value="tbody")[0].find_elements(By.TAG_NAME, value="tr")):
-            next_page(driver,work)
-            time.sleep(3)
-            proxy_num = 0
-
+            if proxy_num == len(driver.find_elements(By.TAG_NAME, value="tbody")[0].find_elements(By.TAG_NAME, value="tr")):
+                next_page(driver, work)
+                proxy_num = 0
+        except Exception as ex:
+            print(ex)
+            work = False
 
 def main():
-    ''''Main function'''
-    parse_page(driver(),work,i)
+    """Main function"""
+    parse_page(driver(), work, i)
+
 
 if __name__ == '__main__':
     main()
